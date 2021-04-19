e4bbda57e050a9675ed1d3ca362173b460ef1671
hngo1@illumina.com <hngo1@illumina.com>
2021-04-19 10:10:46 -0700

    test

--- service/src/main/java/com/chromosome/service/ChromosomeServiceApplication.java
+++ service/src/main/java/com/chromosome/service/ChromosomeServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChromosomeServiceApplication {
-
+     // main 
 	public static void main(String[] args) {
 		SpringApplication.run(ChromosomeServiceApplication.class, args);
 	}
-
+    
 }
